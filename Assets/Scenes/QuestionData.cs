9a555d49016bb6b17069f1211af5f1793c6b8fda
Samu902 <samuelemarrani@gmail.com>
2020-09-20 18:03:30 +0200

    First commit

---
+++ Assets/Scenes/QuestionData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New question", menuName = "Question")]
+public class QuestionData : ScriptableObject
+{
+    public string question;
+    public string correctAnswer;
+    public string[] wrongAnswers;
+
+    public QuestionData()
+    {
+        question = "Question";
+        correctAnswer = "Correct";
+        wrongAnswers = new string[] { "Wrong 1", "Wrong 2", "Wrong 3" };
+    }
+
+    public QuestionData(string question, string correctAnswer, string[] wrongAnswers)
+    {
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.wrongAnswers = wrongAnswers;
+    }
+}
